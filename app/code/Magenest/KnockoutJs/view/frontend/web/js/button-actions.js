e406bea1f2f7e1bc0bb00830c1f11deabf332c05
VanChien411 <phuthienchien3@gmail.com>
2025-05-16 10:34:14 +0700

    Add requireJs form login

--- app/code/Magenest/KnockoutJs/view/frontend/web/js/button-actions.js
+++ app/code/Magenest/KnockoutJs/view/frontend/web/js/button-actions.js
@@ -1,8 +1,9 @@
 define([
     'jquery',
     'uiComponent',
-    'ko'
-], function ($, Component, ko) {
+    'ko',
+    'Magento_Ui/js/modal/modal'
+], function ($, Component, ko, modal) {
     'use strict';
 
     return Component.extend({
@@ -11,7 +12,24 @@ define([
             return this;
         },
         showAlert: function () {
-            alert('Hello World!');
+            var options = {
+                type: 'popup',
+                responsive: true,
+                title: 'Hello World',
+                buttons: []
+            };
+            var popup = modal(options, $('#hello-modal')); // Store the modal instance
+            $('#hello-modal').modal('openModal');
+        },
+        showLoginModal: function () {
+            var options = {
+                type: 'popup',
+                responsive: true,
+                title: 'Login',
+                buttons: []
+            };
+            var popup = modal(options, $('#login-modal')); // Store the modal instance
+            $('#login-modal').modal('openModal');
         }
     });
 });
